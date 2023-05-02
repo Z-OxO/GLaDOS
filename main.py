925a534343f0ed18517b9ebaec94cbce6891c937
Z-OxO <z.oxo.sec@proton.me>
2023-05-02 19:31:39 +0200

    GLaDOS can now talk

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import configparser
 from colorama import Fore
 from pyaudio import *
 import speech_recognition as sr
-import pyaudio
+import pyttsx3
 
 
 # Charge la clé API OpenAI depuis la configuration
@@ -15,8 +15,16 @@ openai.api_key = config["openai"]["api_key"]
 def Menu():
     pass
 
+
+def text_to_speech(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
 def Gpt_turbo():
     content = audio_reconize()
+    print("GLaDOS thinks ...")
     completion = openai.ChatCompletion.create(
     model = 'gpt-3.5-turbo',
     messages = [
@@ -24,8 +32,9 @@ def Gpt_turbo():
     ],
     temperature = 0  
     )
-    print(Fore.LIGHTMAGENTA_EX+"GLaDOS ----> "+completion['choices'][0]['message']['content']+Fore.RESET)
-    choice = input("Another request? (y or n)")
+    completion = completion['choices'][0]['message']['content']
+    print(Fore.LIGHTMAGENTA_EX+"GLaDOS ----> ",completion+Fore.RESET)
+    text_to_speech(completion)
     while True:
         choice = input("Another request? (y or n)")
         if choice == "y":
@@ -44,11 +53,18 @@ def audio_reconize():
     # Conversion audio en texte
     try:
         text = r.recognize_google(audio, language='fr-FR')
-        print(Fore.RED,"You said : " + text)
-        return text
+        print(Fore.LIGHTCYAN_EX,"You said : " + text)
+        while True:
+            choice_audio=""
+            choice_audio = input("Submit to GLaDOS ? (y or n): ")
+            if choice_audio == "y":
+                return text
+            elif choice_audio =="n":
+                audio_reconize()
     except sr.UnknownValueError:
-        print(Fore.RED,"I did not understand what you said.")
+        print(Fore.LIGHTRED_EX,"GLaDOS did not understand what you said. Retry please",Fore.RESET)
+        Gpt_turbo()
     except sr.RequestError as e:
-        print(Fore.RED,"Error when requesting the speech recognition service :{0}".format(e))
+        exit(Fore.LIGHTRED_EX,"Error when requesting the speech recognition service :{0}".format(e))
         
 Gpt_turbo()
